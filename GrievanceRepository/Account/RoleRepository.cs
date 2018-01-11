9cc163b512dc94d822c32ec3863bd98b87f5009b
mosGada <mosgada@gmail.com>
2018-01-11 19:08:43 +0200

    Get Roles on Account Controller/API

---
+++ GrievanceRepository/Account/RoleRepository.cs
@@ -0,0 +1,45 @@
+﻿using Grievance.DAL;
+using Grievance.DAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrievanceRepository.Account
+{
+    public class RoleRepository
+    {
+        #region Private member variable(s)
+        private grievancedbEntities _ctx;
+        #endregion
+
+        public RoleRepository()
+        {
+            _ctx = new grievancedbEntities();
+        }
+
+        /// <summary>
+        /// Get Roles
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<RoleDTO>> GetAll()
+        {
+            var roleDTO = new List<RoleDTO>();
+            await Task.Run(() =>
+            {
+                var roles = _ctx.AspNetRoles.ToList();
+                if (roles.Any())
+                {
+                    roleDTO.AddRange(roles.Select(role => new RoleDTO()
+                    {
+                        Id = role.Id,
+                        Name = role.Name
+                    }));
+                }
+            });
+
+            return roleDTO;
+        }
+    }
+}
