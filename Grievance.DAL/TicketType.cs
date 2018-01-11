bb5e82c4cd7747b001f0c1ee693983b4550e68d6
kokswelma <32953054+kokswelma@users.noreply.github.com>
2018-01-11 19:05:59 +0200

    Removed TicketOwner Table

--- Grievance.DAL/TicketType.cs
+++ Grievance.DAL/TicketType.cs
@@ -17,7 +17,6 @@ namespace Grievance.DAL
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public TicketType()
         {
-            this.TicketOwners = new HashSet<TicketOwner>();
             this.Tickets = new HashSet<Ticket>();
         }
     
@@ -29,8 +28,6 @@ namespace Grievance.DAL
         public string UpdatedBy { get; set; }
         public Nullable<System.DateTime> UpdatedDate { get; set; }
     
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<TicketOwner> TicketOwners { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Ticket> Tickets { get; set; }
     }
